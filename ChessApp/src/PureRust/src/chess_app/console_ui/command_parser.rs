50f943028f399414cde4be475fc2e01b4288be5b
Igor Bespalchuk <20813479+iamhere2@users.noreply.github.com>
2022-10-22 01:01:25 +0300

    minor test addons

--- ChessApp/src/PureRust/src/chess_app/console_ui/command_parser.rs
+++ ChessApp/src/PureRust/src/chess_app/console_ui/command_parser.rs
@@ -126,6 +126,8 @@ mod tests {
         assert_eq!(parse("List"), Ok(Command::ListGames));
         assert!(matches!(parse("NotACommand"), Err(Error(_))));
         assert_eq!(parse("load AAA"), Ok(Command::LoadGame("AAA".to_string())));
+        assert_eq!(parse("save AAA"), Ok(Command::SaveGame("AAA".to_string())));
+        assert_eq!(parse("del AAA"), Ok(Command::DeleteGame("AAA".to_string())));
         assert_eq!(parse("new Black"), Ok(Command::NewGame(Color::Black)));
         assert_eq!(parse("E2 - E4"), Ok(Command::MakeTurn(Turn::Move(Cell::at('E', 2), Cell::at('E', 4)))));
         assert_eq!(parse("g3-h7"), Ok(Command::MakeTurn(Turn::Move(Cell::at('G', 3), Cell::at('H', 7)))));
