344f16d02e277937e2d40d003b0ef0c7dbfc94eb
Igor Bespalchuk <20813479+iamhere2@users.noreply.github.com>
2022-10-23 15:45:33 +0300

    file_io tests

--- ChessApp/src/PureRust/src/hlcd_infra/file_io/tests.rs
+++ ChessApp/src/PureRust/src/hlcd_infra/file_io/tests.rs
@@ -9,3 +9,20 @@ fn test_get_current_directory() {
     let io = get_file_io();
     assert!(io.get_current_directory().unwrap().len() > 0);
 }
+
+#[test]
+fn test_list_read_write_delete_cyle() {
+    let io = get_file_io();
+    let cur_dir = io.get_current_directory().unwrap();
+    
+    let tmp_file = "__test_tmp_file.txt";
+    let tmp_file_pattern = "__test_tmp_fil?.*";
+    let content = "Test Content ЯЫЁЖ";
+
+    assert_eq!(io.list_files(cur_dir.as_str(), tmp_file_pattern).unwrap(), Vec::<String>::new());
+    assert_eq!(io.write_file(tmp_file, content).unwrap(), ());
+    assert_eq!(io.list_files(cur_dir.as_str(), tmp_file_pattern).unwrap(), vec![tmp_file]);
+    assert_eq!(io.read_file(tmp_file).unwrap(), content);
+    assert_eq!(io.delete_file(tmp_file).unwrap(), ());
+    assert_eq!(io.list_files(cur_dir.as_str(), tmp_file_pattern).unwrap(), Vec::<String>::new());
+}
