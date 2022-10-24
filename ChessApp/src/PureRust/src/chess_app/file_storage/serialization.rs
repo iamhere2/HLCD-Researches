cc1e086ae1e93e3b37425a5851496df6b3acdaae
Igor Bespalchuk <20813479+iamhere2@users.noreply.github.com>
2022-10-24 23:55:03 +0300

    color serde

--- ChessApp/src/PureRust/src/chess_app/file_storage/serialization.rs
+++ ChessApp/src/PureRust/src/chess_app/file_storage/serialization.rs
@@ -1,8 +1,10 @@
 #[cfg(test)]
 mod tests;
 
+use std::str::FromStr;
+
 use crate::chess_app::data::{GameHistory, Color, game_history};
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, de::Visitor};
 
 #[derive(Clone, Debug)]
 pub(super) struct Error(pub String);
@@ -29,7 +31,22 @@ impl Serialize for Color {
 impl<'de> Deserialize<'de> for Color {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where D: serde::Deserializer<'de> {
-        todo!()
+        deserializer.deserialize_string(ColorVisitor { })
+    }
+}
+
+struct ColorVisitor { }
+
+impl<'de> Visitor<'de> for ColorVisitor {
+    type Value = Color;
+
+    fn expecting(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        f.write_str("color name")
+    }
+
+    fn visit_str<E>(self, value: &str) -> Result<Color, E>
+    where E: serde::de::Error {
+        Color::try_from(value).map_err(|_|E::custom("wrong color value"))
     }
 }
 
--- ChessApp/src/PureRust/src/chess_app/file_storage/serialization/tests.rs
+++ ChessApp/src/PureRust/src/chess_app/file_storage/serialization/tests.rs
@@ -2,5 +2,9 @@ use crate::chess_app::data::Color;
 
 #[test]
 fn test_color_serde() {
-    assert_eq!(serde_json::to_string(&Color::Black).unwrap(), "\"Black\"");
+    let s = serde_json::to_string(&Color::Black).unwrap();
+    assert_eq!(s, "\"Black\"");
+
+    let c: Color = serde_json::from_str(&s).unwrap();
+    assert_eq!(c, Color::Black);
 }
