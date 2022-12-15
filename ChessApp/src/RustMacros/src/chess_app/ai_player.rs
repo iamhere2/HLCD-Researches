b3963de71cfb45913327730867e65edc3110df25
Igor Bespalchuk <20813479+iamhere2@users.noreply.github.com>
2022-12-15 23:36:26 +0300

    attempt to make ai minimally smart

--- ChessApp/src/RustMacros/src/chess_app/ai_player.rs
+++ ChessApp/src/RustMacros/src/chess_app/ai_player.rs
@@ -1,5 +1,5 @@
+use crate::chess_app::data::Piece;
 use super::{player_interface::*, data::{Turn, GameHistory}, rules_engine::interface::*};
-use rand::prelude::*;
 
 hlcd::define! {
     component StupidAiPlayer {
@@ -7,11 +7,97 @@ hlcd::define! {
         provides { PassiveSyncPlayer }
         impl PassiveSyncPlayer {
             fn turn_request(&self, gh: &GameHistory) -> Turn {
+                let state = gh.current_state();
+                let player = state.next_player_color();
                 let valid_turns = self.rules_engine().get_valid_turns(gh);
-                let mut rng = thread_rng();
-                let rnd_ndx = rng.gen_range(0..valid_turns.len());
-                *valid_turns.get(rnd_ndx).unwrap()
+                valid_turns.into_iter().max_by_key(|t|{
+                    let board_after = { self.rules_engine().apply(state, player, *t).unwrap() };
+                    let gh_after = gh.with(*t, board_after, false);    
+                    self.get_2nd_turn_position_rank(&gh_after)
+                }).unwrap()
             }
         }
+
+        impl {
+            fn get_2nd_turn_position_rank(&self, gh: &GameHistory) -> i32 {
+                let board = gh.current_state();
+                let player = board.next_player_color();
+                let valid_opponent_turns = { self.rules_engine().get_valid_turns(gh) };
+                valid_opponent_turns.into_iter().map(|t|{
+                    let board_after = { self.rules_engine().apply(board, player, t).unwrap() };
+                    let gh_after = gh.with(t, board_after, false);
+                    self.get_position_rank(&gh_after)
+                }).max().unwrap()
+            }
+
+            #[rustfmt::skip]
+            fn get_position_rank(&self, gh: &GameHistory) -> i32 {
+                use Piece::*;
+                let board = gh.current_state();
+                let other_player = board.next_player_color();
+                let figures_rank = board.figures().values().into_iter().fold(0, |acc, (f, c)|
+                    acc + match f {
+                        King   => 0,
+                        Queen  => 10,
+                        Bishop => 8,
+                        Rook   => 7,
+                        Knight => 5,
+                        Pawn   => 1
+                    } * (if *c == other_player { 1 } else { -1 })
+                );
+                
+                let conditions = self.rules_engine().get_conditions(gh);
+                let conditions_rank = conditions.into_iter().fold(0, |acc, (cond, c)|
+                    acc + match cond {
+                        Condition::Check => 100,
+                        Condition::Mate => 1000,
+                    } * (if c == other_player { -1 } else { 1 })
+                );
+
+                figures_rank + conditions_rank
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::chess_app::{rules_engine::{component::RulesEngine, interface::RulesEngineProvider}, data::{board, GameHistory, Cell, Piece, Color}, ai_player::StupidAiPlayer};
+
+    #[test]
+    fn initial_position_rank_is_zero() {
+        let board = board::classic_initial();
+        let gh = GameHistory::new(board, false);
+        let rules_engine = RulesEngineProvider::get(RulesEngine::new());
+        let ai = StupidAiPlayer::new(&rules_engine);
+        let rank = ai.borrow().get_position_rank(&gh);
+        assert_eq!(rank, 0);
+    }
+
+    #[test]
+    fn more_figures_means_better_rank() {
+        let board = board::empty()
+            .with(Piece::Bishop, Color::White, Cell::at('A', 1))
+            .with(Piece::Pawn, Color::Black, Cell::at('H', 8))
+            .with_next_player(Color::White);
+        let gh = GameHistory::new(board, false);
+        let rules_engine = RulesEngineProvider::get(RulesEngine::new());
+        let ai = StupidAiPlayer::new(&rules_engine);
+        let rank = ai.borrow().get_position_rank(&gh);
+        assert_eq!(rank, 7);
+    }
+
+    #[test]
+    fn check_means_better_rank() {
+        let board = board::empty()
+            .with(Piece::Rook, Color::White, Cell::at('A', 1))
+            .with(Piece::Rook, Color::Black, Cell::at('H', 8))
+            .with(Piece::King, Color::Black, Cell::at('A', 8))
+            .with_next_player(Color::White);
+        let gh = GameHistory::new(board, false);
+        let rules_engine = RulesEngineProvider::get(RulesEngine::new());
+        let ai = StupidAiPlayer::new(&rules_engine);
+        let rank = ai.borrow().get_position_rank(&gh);
+        assert_eq!(rank, 100);
     }
 }
